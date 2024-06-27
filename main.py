0989851c94dfde18875436ec713ce54de2c52d71
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-06-28 02:55:35 +0530

    added cors for khoj website

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 import random
+from flask_cors import CORS # Import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "https://www.khojcommunity.com"}})
     # Replace this with your actual JSON data or logic to fetch data
 projects = [
     {
